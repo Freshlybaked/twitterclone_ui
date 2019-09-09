efe235ca44684a579c8fd32e5f615e5ef20d86ce
Sam <sam@picoded.com>
2019-09-09 08:54:06 +0800

    Updated ec2 tag script to give instance a name

--- EC2TagUntag.sh
+++ EC2TagUntag.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 RESOURCE_ID=$(aws ec2 describe-instances --region ap-southeast-1 --filters Name=tag:deploy_group,Values=green Name=tag:twitterclone_ui,Values=true --query Reservations[*].Instances[*].InstanceId | jq -r '.[] | .[0]')
-aws ec2 create-tags --region ap-southeast-1 --resources $RESOURCE_ID --tags Key=deploy_group,Value=blue
+aws ec2 create-tags --region ap-southeast-1 --resources $RESOURCE_ID --tags Key=deploy_group,Value=blue Key=Name,Value=twittercloneui
